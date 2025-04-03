69400d9d10997c2cf040e34e6061a5ba50c4db09
Prakriti <prakriti.jewels02@gmail.com>
2025-04-03 07:36:53 +0530

    Revert "Update cors.config.js"

--- config/cors.config.js
+++ config/cors.config.js
@@ -25,10 +25,10 @@ module.exports = {
 	        "https://test.prakriti.one",
 	        "http://test.prakriti.one",
             "https://super.test.prakriti.one",
-	"https://super.test.prakriti.one/",
-            "https://test.super.prakriti.one",
-            "https://test.admin.prakriti.one",
-            "https://test.team.prakriti.one",
+		    "https://super.test.prakriti.one/",
+            "https://super.test.prakriti.one",
+            "https://admin.test.prakriti.one",
+            "https://team.test.prakriti.one",
         ]
     }
 };
