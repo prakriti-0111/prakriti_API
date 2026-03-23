87311f6fa692c89de8763a81ce5d042f5ca8f39b
Rahul srivastava <87309230+Rahulsrivastava0124@users.noreply.github.com>
2026-03-23 13:13:38 +0530

    Refactor UserCollection.js for improved code readability and consistency in formatting

--- app/resources/superadmin/UserCollection.js
+++ app/resources/superadmin/UserCollection.js
@@ -1,41 +1,48 @@
-const { isObject,getFileAbsulatePath, isEmpty, defaultProfileImage } = require("@helpers/helper");
+const {
+  isObject,
+  getFileAbsulatePath,
+  isEmpty,
+  defaultProfileImage,
+} = require("@helpers/helper");
 
 const UserCollection = (data) => {
-    if(isEmpty(data)){
-        return null;
-    }
-
-    if(isObject(data)){
-        return getModelObject(data);
-    }else if(Array.isArray(data)){
-        let arr = [];
-        for(let i = 0; i < data.length; i++){
-            if(!isEmpty(data[i])){
-                arr.push(getModelObject(data[i]));
-            }
-        }
-        return arr;
-    }
-
+  if (isEmpty(data)) {
     return null;
-}
+  }
+
+  if (isObject(data)) {
+    return getModelObject(data);
+  } else if (Array.isArray(data)) {
+    let arr = [];
+    for (let i = 0; i < data.length; i++) {
+      if (!isEmpty(data[i])) {
+        arr.push(getModelObject(data[i]));
+      }
+    }
+    return arr;
+  }
+
+  return null;
+};
 
 const getModelObject = (data) => {
-    return {
-        id: data.id,
-        email: isEmpty(data.email) ? "" : data.email,
-        name: data.name,
-        company_name: data.company_name,
-        mobile: isEmpty(data.mobile) ? "" : data.mobile,
-        image: (!isEmpty(data.profile_image)) ? getFileAbsulatePath(data.profile_image) : defaultProfileImage(),
-        own: data.own,
-        role_name: "Super Admin",
-        country_id: data.country_id,
-        state_id: data.state_id,
-        gst: data.gst
-    }
-}
+  return {
+    id: data.id,
+    email: isEmpty(data.email) ? "" : data.email,
+    name: data.name,
+    company_name: data.company_name,
+    mobile: isEmpty(data.mobile) ? "" : data.mobile,
+    image: !isEmpty(data.profile_image)
+      ? getFileAbsulatePath(data.profile_image)
+      : defaultProfileImage(),
+    own: data.own,
+    role_name: "Super Admin",
+    country_id: data.country_id,
+    state_id: data.state_id,
+    gst: data.gst,
+  };
+};
 
 module.exports = {
-    UserCollection
-}
+  UserCollection,
+};
